20fc7e833005742390d53272d9e6a206fefb08d1
Procfile <adddbgagdrri2000@gmail.com>
2024-08-28 18:05:41 +0300

    Update A

---
+++ A/goodnigth.py
@@ -0,0 +1,35 @@
+from telethon import events
+import phoenix.client
+client = phoenix.client
+import random
+
+gn = ["""
+┌▀█┌▀█┌▀█┌▀█┌▀█┌▀█┌▀█
+┌▀█╔══╗╔══╗╔══╗╔══╗▀█
+┌▀█║╔═╣║╔╗║║╔╗║╚╗╗║▀█
+┌▀█║╚╗║║╚╝║║╚╝║╔╩╝║▀█
+┌▀█╚══╝╚══╝╚══╝╚══╝▀█
+┌▀█┌▀█┌▀█┌▀█┌▀█┌▀█┌▀█
+┌▀█░░░░░░░░░░░░░░░░▀█
+┌▀█░░█▌▌█▐▀▐░▌▀█▀░░▀█
+┌▀█░░█▐▌█▐▐▐▀▌░█░░░▀█
+┌▀█░░█░▌█▐█▐░▌░█░░░▀█
+┌▀█░░░░░░░░░░░░░░░░▀█
+┌▀█┌▀█┌▀█┌▀█┌▀█┌▀█┌▀█ 
+""",
+"""
+🌙.     *       ☄️      
+🌟   .  *       .         
+                       *   .      🛰     .        ✨      *
+  .     *   SLEEP WELL        🚀     
+      .              . . SWEET DREAMS 🌙
+. *       🌏 GOOD NIGHT         *
+                     🌙.     *       ☄️      
+🌟   .  *       .         
+                       *   .      🛰     .        ✨      *
+"""]
+
+@events.register(events.NewMessage(pattern=".نام"))
+async def goodnight(event):
+  ggn = random.choice(gn)
+  return await event.edit(ggn)
