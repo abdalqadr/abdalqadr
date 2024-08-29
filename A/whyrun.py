8e57086bfcd3b2603d72c2b1821fda11ffc61b8e
Procfile <adddbgagdrri2000@gmail.com>
2024-08-29 15:24:20 +0300

    Update whyrun.py

--- A/whyrun.py
+++ A/whyrun.py
@@ -1,7 +1,7 @@
 from telethon import events
-import phoenix.client
+import A.client
 import random
-client = phoenix.client.client
+client = A.client.client
 
 whylist = ["""Nega Otabek borku Onabek yoq 😂❓ Nega""","""Nega uyg'otadigon soat boru uxlatadigon soat yoq ❓ Nega""","""Nega Ostiruvchi dori bor -u pasaytiruvchi dori yoq Nega 😂""","""Nega Akademiya boru Ukademiya yoq 😜 Nega""","""Nega Temir ismi boru Mis ismi yoq 🤔 Nega""","""Nega Calkulayotir boru Calyiglayotor yoq 💇‍♂️ Nega""","""Nega Kundosh bor Nega Tundosh yoq Nega 😂😂""", """Nega Oshqozonda Qozon yoq Nega 🍭""", """Nega Boychechak boy emas 🌻 Nega""", """Nega Tuyaqush va tuya aka uka emas Nega 🐫""", """Nega qorqiz boruu yomgir qiz yoq nega 🌧️""","""Nega Mandarin boru Sandarin yoq Nega 🤪""","""Nega Nega """, """❓""", """Nega"""]
 
