0843e23e1cae16b2aba7530d8328feb8de28a98c
Procfile <adddbgagdrri2000@gmail.com>
2024-08-29 15:57:32 +0300

    Delete A directory

--- A/fuke.py
+++
@@ -1,7 +0,0 @@
-from telethon import events
-import A.client
-client = A.client.client
-
-@events.register(events.NewMessage(pattern=".احم"))
-async def fuck(event):
-	await event.edit("┏━┳┳┳━┳┳┓\n┃━┫┃┃┏┫━┫┏┓\n┃┏┫┃┃┗┫┃┃┃┃\n┗┛┗━┻━┻┻┛┃┃\n┏┳┳━┳┳┳┓┏┫┣┳┓\n┣┓┃┃┃┃┣┫┃┏┻┻┫\n┃┃┃┃┃┃┃┃┣┻┫┃┃\n┗━┻━┻━┻┛┗━━━┛")
