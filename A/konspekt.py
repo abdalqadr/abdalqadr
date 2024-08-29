60e5966a6a0a87197bf2adeed738ea92ec1c51c1
Procfile <adddbgagdrri2000@gmail.com>
2024-08-29 15:33:18 +0300

    Update konspekt.py

--- A/konspekt.py
+++ A/konspekt.py
@@ -1,6 +1,6 @@
 from telethon import events
-import phoenix.client
-client = phoenix.client.client
+import A.client
+client = A.client.client
 
 
 @events.register(events.NewMessage(outgoing=True, pattern='\.ملخص'))
